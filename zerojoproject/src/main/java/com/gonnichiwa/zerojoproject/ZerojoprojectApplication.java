3e624343b036febde3acb06c79f884856482a088
정재훈 <keepthefaith00@gmail.com>
2020-07-17 09:53:16 +0900

    - android project 생성 - spring boot project 생성

---
+++ zerojoproject/src/main/java/com/gonnichiwa/zerojoproject/ZerojoprojectApplication.java
@@ -0,0 +1,13 @@
+package com.gonnichiwa.zerojoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerojoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerojoprojectApplication.class, args);
+	}
+
+}
---
+++ zerojoproject/src/test/java/com/gonnichiwa/zerojoproject/ZerojoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonnichiwa.zerojoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerojoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
